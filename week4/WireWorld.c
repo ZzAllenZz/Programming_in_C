f64801790c482f0f51ac5c9b2c6dc3fecdf4fd90
ZzAllenZz <44937834+ZzAllenZz@users.noreply.github.com>
2019-10-21 22:42:19 +0100

    Bristol Course

---
+++ week4/WireWorld.c
@@ -0,0 +1,116 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define LEN 40
+void file2array(char *filename,char first_generation[LEN][LEN]);
+void next_state(char first_generation[LEN][LEN],char next_generation[LEN][LEN]);
+int surronding_cells(char generation[LEN][LEN],int row,int column) ;
+int main(int argc,char **argv)
+{
+    char first_generation[LEN][LEN];
+    char *filename = "wirewcircuit1.txt";
+    char next_generation[LEN][LEN];
+    int i,j,k,n=0;
+	file2array(filename,first_generation);
+
+    for(i=0;i<LEN;i++){
+        for(j=0;j<LEN;j++){
+            printf("%c",first_generation[i][j]);
+        }
+        printf("\n");
+    }
+  
+for(k=0;k<10;k++){
+
+	next_state(first_generation,next_generation);
+
+   for(i=0;i<LEN;i++){
+        for(j=0;j<LEN;j++){
+            printf("%c",next_generation[i][j]);
+        }
+        printf("\n");
+    }
+    for(i=0;i<LEN;i++){
+        for(j=0;j<LEN;j++){
+	        first_generation[i][j] = next_generation[i][j];
+        }
+    }
+}
+
+    return 0;
+}
+
+void file2array(char *filename,char first_generation[LEN][LEN])
+{
+    FILE *fp;
+    int i,j,c;
+    
+    if((fp = fopen(filename,"r"))==NULL){
+        printf("Cannot open file named %s\n",filename);
+        exit(1);
+    }
+    
+    for(i=0;i<LEN;i++){
+        for(j=0;j<LEN;){
+            if((c=getc(fp))!='\n'){
+            first_generation[i][j] = c;
+			j++;	
+			}
+		}
+	}
+	fclose(fp);
+}
+
+void next_state(char first_generation[LEN][LEN],char next_generation[LEN][LEN])
+{
+	int i,j,n=0;
+	for(i=0;i<LEN;i++){
+		for(j=0;j<LEN;j++){
+			if(first_generation[i][j]==' '){
+				next_generation[i][j]=' ';
+			}else if(first_generation[i][j]=='H'){
+				next_generation[i][j]='t';
+			}else if(first_generation[i][j]=='t'){
+				next_generation[i][j]='c';
+			}else if(first_generation[i][j]=='c'){
+				n=surronding_cells(first_generation,i,j);
+				if(n==1||n==2){
+					next_generation[i][j]='H';
+				}else{
+					next_generation[i][j]='c';
+				}
+			}else{
+				printf("char error\n");
+				exit(1);
+			}
+		}
+	}
+}
+int surronding_cells(char generation[LEN][LEN],int row,int column){
+	int n=0;
+	if(generation[row][column-1]=='H'){
+		n++;
+	}
+	if(generation[row][column+1]=='H'){
+		n++;
+	}
+	if(generation[row-1][column-1]=='H'){
+		n++;
+	}
+		if(generation[row-1][column]=='H'){
+		n++;
+	}
+		if(generation[row-1][column+1]=='H'){
+		n++;
+	}
+		if(generation[row+1][column-1]=='H'){
+		n++;
+	}
+		if(generation[row+1][column]=='H'){
+		n++;
+	}
+		if(generation[row+1][column+1]=='H'){
+		n++;
+	}
+	return n;
+}
+
---
+++ week4/what.c
@@ -0,0 +1,100 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<malloc.h>
+/*
+用户不断输入数字
+程序将输入的数字从小到大排序,并且能够删除
+体现单链表的优势
+*/
+struct Node{
+	int value;
+	struct Node *next;
+};
+void addNode(struct Node **head,int value);
+void deleteNode(struct Node **head,int value); 
+void printNode(struct Node *head);
+int main(void)
+{
+	struct Node *head = NULL;
+	int input;
+	printf("-----this is for insert integer-----\n");
+	while(1){
+		printf("please enter a integer(-1 for exit):");
+		scanf("%d",&input);
+		if(input==-1){
+			break;
+		}
+		addNode(&head,input);
+		printNode(head);
+	}
+	printf("-----this is for delete integer-----\n");
+	while(1){
+		printf("please enter a integer you want to delete(-1 for exit):");
+		scanf("%d",&input);
+		if(input==-1){
+			break;
+		}
+		deleteNode(&head,input);
+		printNode(head);
+	}
+	return 0; 
+} 
+void addNode(struct Node **head, int value){
+	struct Node *current;
+	struct Node *previous;
+	struct Node *newOne;
+
+	current = *head;
+	previous= NULL;
+
+/*
+��current��ָ��null&&current�е�valueС��input��valueʱ�������
+ֱ��1.currentָ��null �����һ��Ԫ���ˣ�
+or	2.current �е�value����input�е�value�ˡ� 
+*/ 
+	while(current != NULL&&current->value<value){
+		previous = current;
+		current = current->next;
+	} 
+	newOne = (struct Node *)malloc(sizeof(struct Node)); 
+	if(newOne == NULL){
+		printf("Allocation Failed....\n");
+		exit(1);
+	}
+	newOne->value = value;
+	newOne->next=current;
+	if(previous == NULL){
+		*head = newOne;
+	}else {
+		previous->next=newOne;
+
+	}
+
+}
+void deleteNode(struct Node **head,int value){
+	struct Node *previous = NULL;
+	struct Node *current = *head;
+	while(current != NULL && current->value!=value){
+		previous=current;
+		current=current->next;
+	}
+	if(current==NULL){
+		printf("it cannot be find\n");
+		return;
+	} else {
+		if(previous == NULL){
+		*head =current->next;
+	}else{
+		previous->next= current->next;
+	}
+	}
+
+} 
+void printNode(struct Node *head){
+	struct Node *current=head;
+	while(current !=NULL){
+		printf("%d ",current->value);
+		current = current->next;
+	}
+	putchar('\n');
+}
