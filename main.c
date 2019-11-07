f30cff152d8f6c568f3534dc7c9f0a5af55435b2
ZzAllenZz <44937834+ZzAllenZz@users.noreply.github.com>
2019-11-07 11:55:18 +0000

    Bristol Course

--- main.c
+++ main.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
-#define SIZE 81
-/*
- require: log3(SIZE) must be a integer!;
-*/
+#define SIZE 128
+
 typedef struct coordinate{
     int y;
     int x;
@@ -13,12 +11,12 @@ void fulfill(int array[SIZE][SIZE],Co lo,int size);
 
 int main(void){
     int array[SIZE][SIZE];
+    int i,j;
     Co lo;
-    /*
-    use top-left corner to present the center of squares
-    */
-    lo.y=SIZE/3;//: 81/3 = 27;
-    lo.x=SIZE/3;//: 81/3 = 27;
+
+    lo.y=0;
+    lo.x=0;
+
     initialize_array(array);
     printf("initial pattern:\n");
     print_array(array);
@@ -32,7 +30,7 @@ void initialize_array(int array[SIZE][SIZE]){
     int i,j;
     for(i=0;i<SIZE;i++){
         for(j=0;j<SIZE;j++){
-            array[i][j] = '#';
+            array[i][j] = '*';
         }
     }
     return ;
@@ -53,44 +51,36 @@ void print_array(int array[SIZE][SIZE]){
 void fulfill(int array[SIZE][SIZE],Co lo, int size){
 
     int i,j,k;
-    Co newlo;
-    /*!!!
-    Y[9] and X[9] need to be matched, and used in loop.
-    |
-    |
-    |
-    ---->
-    */
-    int Y[9] = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
-    int X[9] = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
-
-    if(size==1){
+    Co newlo_1;
+    Co newlo_2;
+    Co newlo_3;
+    if(size<=4){
         return;
     }
 
-
-    /*when i = 4, X[4],Y[4] = 0,
-    so it present the center of current squares.
-    so we need to asign ' ' to matched array.
-    */
-    for(i=0;i<9;i++){
-        if(i==4){
-            for(j=0;j<(size)/3;j++){
-                for(k=0;k<(size)/3;k++){
-                    array[lo.x+j][lo.y+k] = ' ';
-                    continue;/*not necessray~*/
-                }
-            }
+    for(i=0;i<size/2;i++){
+        for(j=0;j<size/4;j++){
+            array[lo.y+i][lo.x+j] = ' ';
         }
-        /*find the center of subsquares and use the top-left one to represent it. */
-        newlo.y=lo.y+Y[i]*size/3+size/9;
-        newlo.x=lo.x+X[i]*size/3+size/9;
-        fulfill(array,newlo,size/3);
     }
-    /*
-    recursion is useful because:
-    everytime, when we find a new subsquare(call the fulfill function), we repeated the same step:
-    divided into 9 parts, and print the centred one;
-    */
+    for(i=0;i<size/2;i++){
+        for(j=3*size/4;j<size;j++){
+            array[lo.y+i][lo.x+j] = ' ';
+        }
+    }
+    newlo_1.y = lo.y;
+    newlo_1.x = lo.x+size/4;
+    newlo_2.y=lo.y+size/2;
+    newlo_2.x=lo.x;
+    newlo_3.y=lo.y+size/2;
+    newlo_3.x=lo.x+size/2;
+    fulfill(array,newlo_1,size/2);
+    fulfill(array,newlo_2,size/2);
+    fulfill(array,newlo_3,size/2);
+
+
+
+
+
 
 }
